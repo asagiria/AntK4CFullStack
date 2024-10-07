03017a3eaa75fe4a9dc63b488e3c153552744aa9
Antoni Kędziorski <kedziorskia@tlimc.local>
2024-10-07 13:53:45 +0200

    removed .css and added header,content,total in app.jsx

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,28 +1,43 @@
-const Hello = (props) => {
-
-  console.log(props)
+const Header = (props) => {
   return (
     <div>
-      <p>
+      <h1>{props.course}</h1>
+    </div>
+  )
+}
 
-        Hello {props.name}, you are {props.age} years old
-      </p>
+const Content = (props) => {
+  return (
+    <div>
+      <p>{props.part} {props.exercises}</p>
+    </div>
+  )
+}
+
+const Total = (props) => {
+  return (
+    <div>
+      <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
     </div>
   )
 }
 
 const App = () => {
-
-  const friends = [
-    { name: 'Peter', age: 4 },
-    { name: 'Maya', age: 10 },
-  ]
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
 
   return (
     <div>
-      <h1>Greetings</h1>
-      <p>{friends[0].name} {friends[0].age}</p>
-      <p>{friends[1].name} {friends[1].age}</p>
+      <Header course={course}/>
+      <Content part={part1} exercises={exercises1}/>
+      <Content part={part2} exercises={exercises2}/>
+      <Content part={part3} exercises={exercises3}/>
+      <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3}/>
     </div>
   )
 }
